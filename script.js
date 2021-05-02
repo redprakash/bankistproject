d5ccee130724ce89ed84d27548e1de619aae2ce7
Prakash Poudel <bhaktpoudel@gmail.com>
2021-05-02 15:26:52 +1000

    Creating menu fade animation with passing arguments to event handlers

--- script.js
+++ script.js
@@ -98,6 +98,25 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const nav = document.querySelector('.nav');
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//passing an "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 /* JavaScript Events 
 const h1 = document.querySelector('h1');
 const alerth1 = function(e){
