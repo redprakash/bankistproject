0a64ec9bcad647ce17376f7ab64f7aa35e4dfbf6
Prakash Poudel <bhaktpoudel@gmail.com>
2021-05-04 08:07:13 +1000

    Sticky navigation using Intersection Observer API

--- script.js
+++ script.js
@@ -117,6 +117,37 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation using Intersection Observer API
+/*const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
+const obsOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(section1);
+*/
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+  const [entry] = entries; //Destructruing
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0.2,
+  rootMargin: `-${navHeight}`,
+});
+headerObserver.observe(header);
+
 /* JavaScript Events 
 const h1 = document.querySelector('h1');
 const alerth1 = function(e){
